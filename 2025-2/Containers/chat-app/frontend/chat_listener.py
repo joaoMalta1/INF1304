d38682a7d8d651f4c0f5d269063761170a7f4f93
unknown <joaomaltar14@gmail.com>
2025-09-03 19:32:00 -0300

    teste python resiliente v1

--- 2025-2/Containers/chat-app/frontend/chat_listener.py
+++ 2025-2/Containers/chat-app/frontend/chat_listener.py
@@ -1,7 +1,12 @@
 import websocket
+import time
 
-# Endereço do servidor WebSocket do ChatConsumer
-WS_SERVER = "ws://localhost:8081/chat/ws"
+
+WS_SERVERS = [
+    "ws://localhost:8081/chat/ws",
+    "ws://localhost:8082/chat/ws",
+    "ws://localhost:8083/chat/ws"
+]
 
 def on_open(ws):
     print("[INFO] Conectado ao ChatConsumer (Java).")
@@ -20,13 +25,24 @@ def on_close(ws, close_status_code, close_msg):
     print("[INFO] Conexão fechada.")
     return
 
+def connect():
+    server_index = 0
+    while True:
+        server = WS_SERVERS[server_index]
+        print(f"conectar: {server}...")
+        try:
+            ws = websocket.WebSocketApp(
+                server,
+                on_open=on_open,
+                on_message=on_message,
+                on_error=on_error,
+                on_close=on_close
+            )
+            ws.run_forever()
+        except Exception as e:
+            print(f"deu ruim ao conectar no server {server}: {e}")
+        server_index = (server_index + 1) % len(WS_SERVERS)
+        time.sleep(2)
+
 if __name__ == "__main__":
-    websocket.enableTrace(False)
-    ws = websocket.WebSocketApp(
-        WS_SERVER,
-        on_open=on_open,
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close
-    )
-    ws.run_forever()
+    connect()
--- 2025-2/Containers/chat-app/frontend/chat_sender.py
+++ 2025-2/Containers/chat-app/frontend/chat_sender.py
@@ -2,12 +2,15 @@ import websocket
 import json
 import time
 
-# Endereço do servidor WebSocket
-WS_SERVER = "ws://localhost:8080/chat/ws"
+# Lista de servidores WebSocket disponíveis
+WS_SERVER = [
+    "ws://localhost:8080/chat/ws",
+    "ws://localhost:8081/chat/ws",
+    "ws://localhost:8082/chat/ws"
+]
 
-def on_open(ws):
+def chat_loop(ws):
     print("[INFO] Conexão aberta com o servidor WebSocket.")
-    print("Digite suas mensagens abaixo. Para sair, digite 'exit'.")
 
     try:
         while True:
@@ -32,27 +35,38 @@ def on_open(ws):
         print("\n[INFO] Encerrando cliente...")
 
     ws.close()
-    return
+
+def on_open(ws):
+    chat_loop(ws)
 
 def on_message(ws, message):
-    print(f"<<< Recebido do servidor: {message}.")
-    return
+    print(f"<<< Recebido do servidor: {message}")
 
 def on_error(ws, error):
-    print(f"[ERROR] Erro: {error}.")
-    return
+    print(f"[ERROR] Erro: {error}")
 
 def on_close(ws, close_status_code, close_msg):
     print("[INFO] Conexão fechada com o servidor WebSocket.")
-    return
+
+def connect():
+    server_index = 0
+    while True:
+        server = WS_SERVER[server_index]
+        print(f"conectar: {server}...")
+        try:
+            ws = websocket.WebSocketApp(
+                server,
+                on_open=on_open,
+                on_message=on_message,
+                on_error=on_error,
+                on_close=on_close
+            )
+            ws.run_forever()
+        except Exception as e:
+            print(f"deu ruim ao conectar no server {server}: {e}")
+        server_index = (server_index + 1) % len(WS_SERVER)
+        time.sleep(2)
 
 if __name__ == "__main__":
     websocket.enableTrace(False)
-    ws = websocket.WebSocketApp(
-        WS_SERVER,
-        on_open=on_open,
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close
-    )
-    ws.run_forever()
+    connect()
